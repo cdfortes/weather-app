a0a75b991a8284826ce4eaaefce7ff387e5aa132
cdfortes <cdfortes@gmail.com>
2021-07-06 09:30:26 -0100

    create weather app

---
+++ js/weather.js
@@ -0,0 +1,32 @@
+const APIKEY = 'HwutEZ8AbB8KXvWQaWzKWKXpAAUgGPY2'
+const baseURL = `http://dataservice.accuweather.com`
+
+const getCityURL = (cityName) =>
+  `${baseURL}/locations/v1/cities/search?apikey=${APIKEY}&q=${cityName}`
+
+const getCityWeatherURL = ({ Key }) =>
+  `${baseURL}/currentconditions/v1/${Key}?apikey=${APIKEY}&language=pt-br`
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error('Não foi possivel obter os dados')
+    }
+
+    return response.json()
+  } catch ({ name, messsage }) {
+    alert(`${name}: ${messsage}`)
+  }
+}
+
+const getCityData = (cityName) => fetchData(getCityURL(cityName))
+
+const getCityWeatherData = async (cityName) => {
+  const [cityData] = await getCityData(cityName)
+
+  return fetchData(getCityWeatherURL(cityData))
+}
+
+//getCityWeatherData('Mindelo').then(console.log)
